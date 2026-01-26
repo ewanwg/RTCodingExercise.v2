d5f7932f369f8fb5112ec2fd9480b93564d16e45
Ewan Westcott Graham <ewanwg@protonmail.com>
2026-01-26 20:21:45 +0000

    Setup the routes for the new watchlist page, and add a button to the top nav to be able to access it

--- src/Web/WebAngular/src/app/app.routes.ts
+++ src/Web/WebAngular/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PlateListComponent } from './components/plate-list/plate-list';
+import { PlateWatchlist } from './components/plate-watchlist/plate-watchlist';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: PlateListComponent },
+  { path: 'watchlist', component: PlateWatchlist },
+];
--- src/Web/WebAngular/src/app/app.ts
+++ src/Web/WebAngular/src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { PlateListComponent } from './components/plate-list/plate-list';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [PlateListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
