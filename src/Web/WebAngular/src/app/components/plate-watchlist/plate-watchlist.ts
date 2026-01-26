64a863749cc538725857cbc546ee64b0c72140a8
Ewan Westcott Graham <ewanwg@protonmail.com>
2026-01-26 20:36:41 +0000

    Create a very simple table just to return some data from PlatesWatchlist. I'm not initalising this data correctly as if you come off the page and go back to it the table disappears

--- src/Web/WebAngular/src/app/components/plate-watchlist/plate-watchlist.ts
+++ src/Web/WebAngular/src/app/components/plate-watchlist/plate-watchlist.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { PlatesWatchlist } from '../../models/plate-watchlist';
+import { Catalog } from '../../services/catalog';
+import { DecimalPipe, DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-plate-watchlist',
-  imports: [],
+  imports: [DecimalPipe, DatePipe],
   templateUrl: './plate-watchlist.html',
   styleUrl: './plate-watchlist.css',
 })
-export class PlateWatchlist {
+export class PlateWatchlist implements OnInit {
+  watchlist: PlatesWatchlist[] = [];
+  error: string | null = null;
+  loading = false;
+
+  constructor(private catalogService: Catalog, private cdr: ChangeDetectorRef) { }
+  
+  ngOnInit(): void {
+    this.loadWatchlist();
+  }
+
+  loadWatchlist(): void {
+    this.loading = true;
+    this.catalogService.getWatchlist().subscribe({
+      next: (data) => {
+        this.watchlist = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load watchlist.';
+        console.error(err);
+        this.loading = false;
+      }
+    });
+  }
 
 }
